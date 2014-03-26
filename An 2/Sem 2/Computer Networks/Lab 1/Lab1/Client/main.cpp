bbc6be9ab167185c13ea5cdd8dd778f3c56d7d18
Mihai Costea <mihai.andrei.costea@icloud.com>
2014-03-26 14:58:56 +0200

    [CN]Lab 1 implementation

--- An 2/Sem 2/Computer Networks/Lab 1/Lab1/Client/main.cpp
+++ An 2/Sem 2/Computer Networks/Lab 1/Lab1/Client/main.cpp
@@ -8,11 +8,90 @@
 
 #include <iostream>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <vector>
+
+int _client;
+
+void timeOut(int signal) {
+    printf("Time out!\n");
+    close(_client);
+    
+    exit(1);
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    struct sockaddr_in server;
+    _client = socket(AF_INET, SOCK_STREAM, 0);
+    if (_client < 0) {
+        perror("Error creating socket");
+        exit(errno);
+    }
+    
+    memset(&server, 0, sizeof(struct sockaddr_in));
+    server.sin_family = AF_INET;
+    server.sin_port = htons(5555);
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    std::vector<int32_t> string1, string2;
+    uint32_t count1, count2;
+    
+    std::cout << "count1:";
+    std::cin >> count1;
+    std::cout << "count2:";
+    std::cin >> count2;
+    
+    for (int it=0; it<count1; it++) {
+        int32_t no;
+        std::cin >> no;
+        string1.push_back(no);
+    }
+    
+    for (int it=0; it<count2; it++) {
+        int32_t no;
+        std::cin >> no;
+        string2.push_back(no);
+    }
+    
+    if (connect(_client, (struct sockaddr *)&server, sizeof(struct sockaddr_in)) < 0) {
+        perror("Error connecting to server");
+        exit(errno);
+    }
+    
+    send(_client, &count1, sizeof(uint32_t), 0);
+    send(_client, &count2, sizeof(uint32_t), 0);
+    
+    for (int it=0; it<count1; it++) {
+        int32_t no = string1[it];
+        send(_client, &no, sizeof(int32_t), 0);
+    }
+    
+    for (int it=0; it<count2; it++) {
+        int32_t no = string2[it];
+        send(_client, &no, sizeof(int32_t), 0);
+    }
+    
+    uint32_t count;
+    std::vector<int32_t> common;
+    signal(SIGALRM, timeOut);
+    alarm(10);
+    recv(_client, &count, sizeof(uint32_t), 0);
+    for (int it=0; it<count; it++) {
+        int32_t no = 0;
+        alarm(10);
+        recv(_client, &no, sizeof(int32_t), 0);
+        common.push_back(no);
+    }
+    
+    for (std::vector<int32_t>::iterator it=common.begin(); it!=common.end(); ++it) {
+        std::cout << *it << std::endl;
+    }
+    
     return 0;
 }
 
--- An 2/Sem 2/Computer Networks/Lab 1/Lab1/Lab1/main.cpp
+++ An 2/Sem 2/Computer Networks/Lab 1/Lab1/Lab1/main.cpp
@@ -12,33 +12,73 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
-#include "utils.h"
+#include <arpa/inet.h>
+#include <vector>
 
 int             _serverSock;
-threadinfo_t    _workers[5];
+int             _clientSock;
 
-void *workerFunction(void *args) {
-    int *argsPtr = (int *)args;
-    int clientSock = *argsPtr;
+void timeOut(int signal) {
+    int32_t resp = -1;
+    resp = htonl(resp);
+    printf("Time out\n!");
+    send(_clientSock, &resp, sizeof(int32_t), 0);
+    close(_clientSock);
     
-    int count1, count2;
-    recv(clientSock, &count1, 4, 0);
-    
-    return nullptr;
+    exit(1);
 }
 
-void handleClient(int socket) {
-    for (int it = 0; it < 5; it++) {
-        if (!_workers[it].isAllocated) {
-            pthread_create(&_workers[it].thread, nullptr, workerFunction, (void *)&socket);
+void handleClient() {
+    uint32_t count1, count2;
+    signal(SIGALRM, timeOut);
+    alarm(10);
+    recv(_clientSock, &count1, sizeof(uint32_t), 0);
+    alarm(10);
+    recv(_clientSock, &count2, sizeof(uint32_t), 0);
+    alarm(0);
+    
+    std::vector<int32_t> string1, string2;
+    
+    for (int it=0; it<count1; it++) {
+        int32_t no = 0;
+        alarm(10);
+        recv(_clientSock, &no, sizeof(int32_t), 0);
+        string1.push_back(no);
+    }
+    alarm(0);
+    for (int it=0; it<count2; it++) {
+        int32_t no = 0;
+        alarm(10);
+        recv(_clientSock, &no, sizeof(int32_t), 0);
+        string2.push_back(no);
+    }
+    alarm(0);
+    
+    //SUCCESS
+    std::vector<int32_t> common;
+    
+    for (int it=0; it<string1.size(); it++) {
+        for (int jt=0; jt<string2.size(); jt++) {
+            if (string1[it] == string2[jt]) {
+                common.push_back(string1[it]);
+            }
         }
     }
+    
+    int32_t resp = (int32_t)common.size();
+    send(_clientSock, &resp, sizeof(int32_t), 0);
+    
+    for (int it=0; it<common.size(); it++) {
+        send(_clientSock, &common[it], sizeof(int32_t), 0);
+    }
+    
+    close(_clientSock);
+    
+    exit(0);
 }
 
 int main(int argc, const char * argv[])
 {
-    int clientSock;
     struct sockaddr_in serverAddr, clientAddr;
     
     _serverSock = socket(AF_INET, SOCK_STREAM, 0);
@@ -47,26 +87,27 @@ int main(int argc, const char * argv[])
         return errno;
     }
     
-    memset(&serverAddr, 0, sizeof(sockaddr_in));
+    memset(&serverAddr, 0, sizeof(struct sockaddr_in));
     serverAddr.sin_port = htons(5555);
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     
-    if (bind(_serverSock, (struct sockaddr *)&serverAddr, sizeof(sockaddr_in)) < 0) {
+    if (bind(_serverSock, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_in)) < 0) {
         perror("Failed to bind");
         return errno;
     }
     
     listen(_serverSock, 5);
-    socklen_t l;
-    memset(&clientAddr, 0, sizeof(sockaddr_in));
-    l = sizeof(clientAddr);
     while (1) {
-        clientSock = accept(_serverSock, (struct sockaddr *)&clientAddr, (socklen_t *)&l);
+        socklen_t l;
+        memset(&clientAddr, 0, sizeof(struct sockaddr_in));
+        l = sizeof(clientAddr);
+        _clientSock = accept(_serverSock, (struct sockaddr *)&clientAddr, (socklen_t *)&l);
         
-        handleClient(clientSock);
+        if (fork() == 0) {
+            handleClient();
+        }
     }
-    
     
     return 0;
 }
