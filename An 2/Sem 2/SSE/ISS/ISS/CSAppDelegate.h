4ddd57f28944e47de1aad027d7ea32601ced54ac
Mihai Costea <mihai.andrei.costea@icloud.com>
2014-04-27 15:30:49 +0300

    [SSE] Begin implementation

---
+++ An 2/Sem 2/SSE/ISS/ISS/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  ISS
+//
+//  Created by Mihai Costea on 17/04/14.
+//  Copyright (c) 2014 info. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CSAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ An 2/Sem 2/SSE/ISS/ISS/CSAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  CSAppDelegate.m
+//  ISS
+//
+//  Created by Mihai Costea on 17/04/14.
+//  Copyright (c) 2014 info. All rights reserved.
+//
+
+#import "CSAppDelegate.h"
+
+@implementation CSAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
